bf8ff2fe93c6f2dd20c3e8656d22efbe1398ad77
Steph Hoel <steph.hoel@gmail.com>
2023-09-19 13:52:39 -0300

    feat: altera informações, add podcast recomendados

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function toggleMode() {
 
   html.classList.toggle('light')
 
-  const img = document.querySelector('#profile img')
+  // const img = document.querySelector('#profile img')
 
-  if (html.classList.contains('light')) {
-    img.setAttribute('src', './assets/avatar-light.png')
-    img.setAttribute('alt','Foto de Mayk Brito usando óculos escuros, camisa preta, jaqueta preta, sem barba no fundo gradiente do lilás para o azul')
-  } else {
-    img.setAttribute('src', './assets/avatar.png')
-    img.setAttribute('alt','Foto de Mayk Brito usando óculos, camisa preta, barba e fundo laranja')
-  }
+  // if (html.classList.contains('light')) {
+  //   img.setAttribute('src', './assets/avatar-light.png')
+  //   img.setAttribute('alt','Foto de Mayk Brito usando óculos escuros, camisa preta, jaqueta preta, sem barba no fundo gradiente do lilás para o azul')
+  // } else {
+  //   img.setAttribute('src', './assets/avatar.png')
+  //   img.setAttribute('alt','Foto de Mayk Brito usando óculos, camisa preta, barba e fundo laranja')
+  // }
 }
